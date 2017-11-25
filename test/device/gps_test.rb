ab1e5b2954d90893a598bd90509880722801a90c
buzuloiu <pbuzuloiu@yahoo.ca>
2017-11-25 17:29:17 +0000

    small change, beagle bone wouldnt allow for more due to memory, more will be added

--- test/device/gps_test.rb
+++ test/device/gps_test.rb
@@ -1,12 +1,14 @@
 require 'minitest/autorun'
-require_relative '../app/device/gps'
+require_relative '../../app/device/gps'
 
-class GpsTest < MiniTest::Test
+class Device::GpsTest < MiniTest::Test
 
   def setup
   end
 
   def test_read
+  assert_equal(1,gps.new().gps_parse('GPGGA,194530.000,3051.8007,N,10035.9989,W,1,4,2.18,746.4,M,-22.2,M,,*6B'))
+
   end
 
   def test_write
