cb5e98629de745126f762ea3ca99d330df5733d1
Ethan Chan <ethanchan144@gmail.com>
2017-11-24 20:54:56 -0500

    Update gps.rb

--- app/device/gps.rb
+++ app/device/gps.rb
@@ -8,42 +8,42 @@ class Device::GPS
   gps = UARTDevice.new(:UART1, 9600)
    
   def gps_read
-    Nmea = gps.readline
+    nmea = gps.readline
   end
 
-  def gps_parse(Nmea)
-    if Nmea.include?("GPRMC")
-      N1 = Nmea.split(",")
-      U_in = N1[1].index(".")-2
-      p_in = N1[3].index(".")-2
-      p2_in = N1[5].index(".")-2
+  def gps_parse(nmea)
+    if nmea.include?("GPRMC")
+      n1 = nmea.split(",")
+      u_in = n1[1].index(".")-2
+      p_in = n1[3].index(".")-2
+      p2_in = n1[5].index(".")-2
 
-      UT2C = (N1[1][0..U_in])+(N1[1][U_in..-1]/100)
-      Signal_stat = N1[2]
-      Lat1 = L_format(N1[3][0..p_in],N1[3][p_in..-1],N1[4])
-      Long1 = L_format(N1[5][0..p2_in],N1[5][p2_in..-1],N1[6])
-      speed_in_knots = N1[7]
-      track angle= N1[8]
-      DMY = N1[9]
+      utc = (n1[1][0..u_in])+(n1[1][u_in..-1]/100)
+      signal_stat = n1[2]
+      lat1 = l_format(n1[3][0..p_in],n1[3][p_in..-1],n1[4])
+      long1 = l_format(n1[5][0..p2_in],n1[5][p2_in..-1],n1[6])
+      speed_in_knots = n1[7]
+      track_angle= n1[8]
+      dmy = n1[9]
 
-    elsif Nmea.include?("GPGGA")
-      N2 = Nmea.split(",")
-      U_in = N2[1].index(".")-2
-      p_in = N2[2].index(".")-2
-      p2_in = N2[4].index(".")-2
+    elsif nmea.include?("GPGGA")
+      n2 = nmea.split(",")
+      u_in = n2[1].index(".")-2
+      p_in = n2[2].index(".")-2
+      p2_in = n2[4].index(".")-2
 
-      UTC2 = (N2[1][0..U_in])+(N2[1][U_in..-1]/100)
-      Lat2 = L_format(N2[2][0..p_in],N2[2][p_in..-1],N1[3])
-      Long2 = L_format(N2[4][0..p2_in],N2[4][p2_in..-1],N1[5])
-      fix_state = N2[6]
-      Satellites_amount = N2[7]
-      h_dilution = N2[8]
-      Alt = N2[9]
-      A_Sea_level = N2[11]
+      utc2 = (n2[1][0..u_in])+(n2[1][u_in..-1]/100)
+      lat2 = l_format(n2[2][0..p_in],n2[2][p_in..-1],n2[3])
+      long2 = l_format(n2[4][0..p2_in],n2[4][p2_in..-1],n2[5])
+      fix_state = n2[6]
+      satellites_amount = n2[7]
+      h_dilution = n2[8]
+      alt = n2[9]
+      a_sea_level = n2[11]
 
   end
 
-  def L_format(l,minutes,direction)
+  def l_format(l,minutes,direction)
     l_return = l+(minutes/60)
     if direction=='N' or direction=='E'
       pass
