69c8f0718853571ef90cc0ec02ad4a7b50ba8671
NikhilPeri <nikhil.peri@shopify.com>
2017-11-25 11:34:21 -0500

    error free

--- app/device/accelerometer.rb
+++ app/device/accelerometer.rb
@@ -1,35 +1,41 @@
 require 'beaglebone'
 
-class Device::Accelerometer
-  include Beaglebone
+module Device
+  class Accelerometer
+    include Beaglebone
 
-  I2C_ADDR = 0x53
+    I2C_ADDR = 0x53
 
-  def read
-    puts read_i2c(6, 0x1D)
-  end
+    def initialize
+      write_i2c(0x2D, 0x08)
+    end
 
-  def sleep
-    new_register = read_i2c(1, 0x3E) | 0b01000000
-    write(0x3E, new_register)
-  end
+    def read
+      puts read_i2c(6, 0x1D)
+    end
 
-  def wake
-    new_register = read_i2c(1, 0x3E) | 0b01000000
-    write(0x3E, new_register)
-  end
+    def sleep
+      new_register = read_i2c(1, 0x3E) | 0b01000000
+      write(0x3E, new_register)
+    end
 
-  private
+    def wake
+      new_register = read_i2c(1, 0x3E) | 0b01000000
+      write(0x3E, new_register)
+    end
 
-  def write_i2c(register, data)
-    i2c_gyro.write(I2C_ADDR, [register, data].pack('C*'))
-  end
+    private
 
-  def read_i2c(bytes, start_address)
-    i2c_gyro.read(I2C_ADDR, bytes, [start_address].pack('C*'))
-  end
+    def write_i2c(register, data)
+      i2c_gyro.write(I2C_ADDR, [register, data].pack('C*'))
+    end
 
-  def i2c_gyro
-    @i2c_gyro ||= I2CDevice.new(:I2C2)
+    def read_i2c(bytes, start_address)
+      i2c_gyro.read(I2C_ADDR, bytes, [start_address].pack('C*'))
+    end
+
+    def i2c_gyro
+      @i2c_gyro ||= I2CDevice.new(:I2C2)
+    end
   end
 end
