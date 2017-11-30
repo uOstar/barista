89591d0d91fe98c113a6d80bc58f9ed771defa4f
Ethan Chan <ethanchan144@gmail.com>
2017-11-30 14:51:21 -0500

    Update gps_test.rb

--- test/device/gps_test.rb
+++ test/device/gps_test.rb
@@ -10,7 +10,8 @@ class Device::GpsTest < MiniTest::Test
     assert_equal(1,gps.new().gps_parse('GPGGA,194530.000,3051.8007,N,10035.9989,W,1,4,2.18,746.4,M,-22.2,M,,*6B'))
   end
   def test_read
-    assert_equal(1,gps.new().gps_parse(gps.new().gps_read()))
+    g = gps.new()
+    assert_equal(1,g.gps_parse(g.gps_read()))
   end
   
   def test_write
