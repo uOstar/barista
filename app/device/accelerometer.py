95eb6af542ee6fd89918a171098426e3d0b35b60
Paul Buzuloiu <pbuzuloiu@yahoo.ca>
2018-02-10 18:04:18 -0500

    refactoring accelerometer

--- app/device/accelerometer.py
+++ app/device/accelerometer.py
@@ -1,82 +1,75 @@
-from i2c import I2C
+from app.utils.i2c import I2C
 from time import sleep
 from time import time
 import numpy as np
-class Accelerometer(object):
 
-    ADDRESS = 0x53
-    #REGISTERS
-    DEVID = 0x00
-    BW_RATE = 0x2C # Data rate and power mode control
-    POWER_CTL = 0x2D # Power-saving features control
-    DATA_FORMAT = 0x31
-    DATAX0 = 0x32
-    DATAX1 = 0x33
-    DATAY0 = 0x34
-    DATAY1 = 0x35
-    DATAZ0 = 0x36
-    DATAZ1 = 0x37
+
+ADDRESS = 0x53
+#REGISTERS
+DEVID = 0x00
+BW_RATE = 0x2C # Data rate and power mode control
+POWER_CTL = 0x2D # Power-saving features control
+DATA_FORMAT = 0x31
+DATAX0 = 0x32
+DATAX1 = 0x33
+DATAY0 = 0x34
+DATAY1 = 0x35
+DATAZ0 = 0x36
+DATAZ1 = 0x37
+
+
+class Accelerometer(object):
 
 
     def __init__(self):
-        self.i2c = I2C(1, 0x53)
-        #set resolution to 16g
-        self.i2c.clear_bit(self.DATA_FORMAT, 1)
-        self.i2c.clear_bit(self.DATA_FORMAT, 0)
+        self.i2c = I2C(1, ADDRESS)
+        #clearing DATA_FORMAT register
+        self.i2c.write_byte(DATA_FORMAT,0b00000000)
+        #set range to plusminus 8g
+        self.i2c.set_bit(DATA_FORMAT, 1)
+        self.i2c.set_bit(DATA_FORMAT, 0)
         #right justify with sign extension
-        self.i2c.clear_bit(self.DATA_FORMAT, 2)
+        self.i2c.clear_bit(DATA_FORMAT, 2)
         #set maximum resolution mode (4mg/LSB)
-        self.i2c.set_bit(self.DATA_FORMAT, 3)
+        self.i2c.set_bit(DATA_FORMAT, 3)
         #set data rate to 3200Hz
-        self.i2c.write_byte(self.BW_RATE, 0b00001111)
+        self.i2c.write_byte(BW_RATE, 0b00001111)
         #setting the device to measure
-        self.i2c.set_bit(self.POWER_CTL, 3)
+        self.i2c.set_bit(POWER_CTL, 3)
 
 
     def read(self):
-
-        sensor_read = self.i2c.read_block(self.DATAX0,6)
-        #block_read returns a list of int
-
-        raw_data = np.int16([
-            np.bitwise_or(sensor_read[1] << 8, sensor_read[0]),
-            np.bitwise_or(sensor_read[3] << 8, sensor_read[2]),
-            np.bitwise_or(sensor_read[5] << 8, sensor_read[4])
-        ])
-
-        return Accelerometer.parse_raw_data(raw_data)
+        return Accelerometer.parse_raw_data(self.i2c.read_block(DATAX0,6))
 
 
     def sleep(self):
-        self.i2c.set_bit(self.POWER_CTL, 2)
+        self.i2c.set_bit(POWER_CTL, 2)
 
 
     def wake(self):
-        self.i2c.clear_bit(self.POWER_CTL, 3)
-        self.i2c.clear_bit(self.POWER_CTL, 2)
-        self.i2c.set_bit(self.POWER_CTL, 3)
+        self.i2c.clear_bit(POWER_CTL, 3)
+        self.i2c.clear_bit(POWER_CTL, 2)
+        self.i2c.set_bit(POWER_CTL, 3)
 
 
     @staticmethod
     def parse_raw_data(raw_data):
 
-        data = {
-            'x_acceleration': round(raw_data[0]*0.0039,4),
-            'y_acceleration': round(raw_data[1]*0.0039,4),
-            'z_acceleration': round(raw_data[2]*0.0042,4),
+        xyz = np.int16([
+            raw_data[1] << 8 | raw_data[0],
+            raw_data[3] << 8 | raw_data[2],
+            raw_data[5] << 8 | raw_data[4]
+        ])
+
+        return {
+            'x': round(xyz[0]*0.0039,4),
+            'y': round(xyz[1]*0.0039,4),
+            'z': round(xyz[2]*0.0042,4),
             'time':time()
         }
-        return data
 
 
 if __name__ == '__main__':
         accelerometer = Accelerometer()
-    	print "Benchmarking Accelerometer..."
-        start = time()
-        for x in range(0,40000):
-            accelerometer.read()
-        end = time()
-        total = end - start
+        sleep(1)
         print accelerometer.read()
-        average = total/40000
-        print "reading 40000 times took %s seconds total, average of %s seconds per read" %(total,average)
